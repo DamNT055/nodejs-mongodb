da1bfac2247ecab3eefaf4b00e2d0c915c10a958
damnt055 <damntse150556@fpt.edu.vn>
2024-07-21 12:34:04 +0700

    commit code first

---
+++ docker.md
@@ -0,0 +1,5 @@
+# Run Mongodb Dockerd
+
+```
+docker run --name mongodb-container -d -p 27017:27017 mongo
+```
---
+++ index.js
@@ -0,0 +1,77 @@
+import { MongoClient, MongoServerError } from "mongodb";
+const url = "mongodb://0.0.0.0:27017";
+const client = new MongoClient(url);
+const dbName = "myProject";
+
+async function main() {
+  await client.connect();
+  console.log("Connected Successfully to Server");
+  const db = client.db(dbName);
+  const collection = db.collection("documents");
+  return collection;
+}
+
+main()
+  .then(async (collection) => {
+    // Insert Document
+    try {
+      const insertResult = await collection.insertMany([
+        {
+          a: 1,
+        },
+        {
+          b: 2,
+        },
+        {
+          c: 3,
+        },
+      ]);
+      console.log("Inserted Documents", insertResult);
+    } catch (error) {
+      if (error instanceof MongoServerError) {
+        console.log(`Error worth logging: ${error}`);
+      }
+      throw error;
+    }
+
+    // Find Result With a Query filter
+    const filterResult = await collection
+      .find({
+        a: 1,
+      })
+      .toArray();
+    console.log("Find Documents with Filter", filterResult);
+    // Update a Documents
+    const updateResult = await collection.updateOne(
+      { a: 1 },
+      {
+        $set: {
+          b: 5,
+        },
+      }
+    );
+    console.log("Update Documents Result", updateResult);
+    // Create index a collection
+    const indexName = await collection.createIndex({ a: 1 });
+    console.log("Index name", indexName);
+    // Remove Documents
+    const deleteResult = await collection.deleteMany({
+      $or: [
+        {
+          a: 1,
+        },
+        {
+          b: 2,
+        },
+        {
+          c: 3,
+        },
+      ],
+    });
+    console.log("Remove Documents", deleteResult);
+    // Find All Documents
+    const findResult = await collection.find({}).toArray();
+    console.log("Find All Documents", findResult);
+  })
+  .catch(console.error)
+  .finally(() => client.close());
